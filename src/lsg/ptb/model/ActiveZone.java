c7186491a19c02222e4feab4f8450475ef903eca
LesegoLSG <217057620@student.uj.ac.za>
2022-05-08 01:16:33 -0500

    Modified ActiveZone which makes use of regex patterns

--- src/lsg/ptb/model/ActiveZone.java
+++ src/lsg/ptb/model/ActiveZone.java
@@ -1,5 +1,48 @@
 package lsg.ptb.model;
 
-public class ActiveZone {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class ActiveZone extends Zone{
+	
+	public static boolean validate(String line){
+		
+		boolean matched = false;
+		
+		Pattern pattern = Pattern.compile("[A-Z]+\\s\\d+:\\d+\\s[A-Z][a-z]+\\s\\d+");
+		
+		Matcher match = pattern.matcher(line);
+		
+		if(match.matches()){
+			matched =true;
+		}
+		return matched;
+	}
+	
+	private final String export;
+	private final int size;
+	
+	public ActiveZone(int x, int y, EZoneType type, int size, String export){
+		super(x,y,type);
+		this.size = size;
+		this.export = export;
+	}
+	
+	public void accept(IRenderVisitor visitor){
+		visitor.render(this);
+	}
+	
+	public String toString(){
+		return String.format("%s\t%d:%d\t%s\t%d", getType(),getLocation().getX(),getLocation.getY(),getExport(),getSize());
+		
+	}
+	
+	public String getExport(){
+		return export;
+	}
+	
+	public int getSize(){
+		return size;
+	}
 }
+
