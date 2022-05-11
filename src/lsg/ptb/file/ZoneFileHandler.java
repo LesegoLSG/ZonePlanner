d09a5274f34ddfe42f3469a7e253249b9dfbd2be
LesegoLSG <217057620@student.uj.ac.za>
2022-05-11 15:37:59 -0500

    Added comments for a ZoneFileHandler

--- src/lsg/ptb/file/ZoneFileHandler.java
+++ src/lsg/ptb/file/ZoneFileHandler.java
@@ -7,13 +7,24 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-
+	/**
+	 * Class ZoneFileHandler reads data from files
+	 * @author Mhlongo L
+	 *
+	 */
 public final class ZoneFileHandler {
 		
+	/**
+	 * Method takes a file and reads it then returns the zone from the reading
+	 * @param zoneFile, instance of a file to read from
+	 * @return Zone after reading the files
+	 */
 	public static ArrayList<Zone> readZones(File zoneFile){
 		
+		//Creating an instance of Zone
 		ArrayList<Zone> zones = new ArrayList<>();
 		
+		//instantiating a scan to retrieve data
 		Scanner scan = null;
 		
 		try{
@@ -29,6 +40,7 @@ public final class ZoneFileHandler {
 					EZoneType type = EZoneType.valueOf(tokens.nextToken());
 					
 					String loc = tokens.nextToken();
+					//creating a space as a dilemitor
 					StringTokenizer locToc = new StringTokenizer(loc, ": ");
 					int x = Integer.parseInt(locToc.nextToken());
 					int y = Integer.parseInt(locToc.nextToken());
@@ -44,6 +56,7 @@ public final class ZoneFileHandler {
 					EZoneType type = EZoneType.valueOf(tokens.nextToken());
 					
 					String loc = tokens.nextToken();
+					//creating a space as a dilemitor
 					StringTokenizer locToc = new StringTokenizer(loc, ": ");
 					int x = Integer.parseInt(locToc.nextToken());
 					int y = Integer.parseInt(locToc.nextToken());
